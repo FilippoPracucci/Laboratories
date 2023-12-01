521beb2b8a6bca1f04fb1dee2ac854d5a6f2ba4b
Filippo Pracucci - filippo.pracucci@studio.unibo.it <filippo.pracucci@studio.unibo.it>
2023-12-01 12:05:13 +0100

    Some changes in php files

--- Tecnologie_Web/archivio.php
+++ Tecnologie_Web/archivio.php
@@ -2,11 +2,11 @@
 
 require_once("bootstrap.php");
 
-$templateParams["titolo"] = "Blog TW - Contatti";
-$templateParams["nome"] = "contatti.php";
+$templateParams["titolo"] = "Blog TW - Archivio";
+$templateParams["nome"] = "lista-articoli.php";
 $templateParams["articolicasuali"] = $dbh->getRandomPosts(2);
 $templateParams["categorie"] = $dbh->getCategories();
-$templateParams["autori"] = $dbh->getAuthors(2);
+$templateParams["articoli"] = $dbh->getPosts();
 
 require("template/base.php");
 
