47aad0d3c4740b4826f379e628f22a05461b46eb
Filippo Pracucci <filippo.pracucci@studio.unibo.it>
2024-03-19 19:38:41 +0100

    Added xml files for mobile in gitignore. Implementation of HomeScreen, TravelDetailsScreen, MainActivity and AppBar inside TravelDiary

--- Programmazione_Mobile/TravelDiary/app/src/main/java/com/example/traveldiary/ui/screens/HomeScreen.kt
+++ Programmazione_Mobile/TravelDiary/app/src/main/java/com/example/traveldiary/ui/screens/HomeScreen.kt
@@ -1,53 +1,103 @@
 package com.example.traveldiary.ui.screens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Add
+import androidx.compose.material.icons.outlined.Image
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import com.example.traveldiary.AppBar
+import com.example.traveldiary.ui.composables.AppBar
 
 @Composable
-fun TravelDiary() {
-    Column {
-        Scaffold(
-            topBar = { AppBar() }
-        ) { contentPadding ->
-            Text("TravelDiary", Modifier.padding(contentPadding))
+fun HomeScreen() {
+    val items = (1..20).map { "Item n°$it" }
+
+    Scaffold(
+        floatingActionButton = {
+            FloatingActionButton(
+                containerColor = MaterialTheme.colorScheme.primary,
+                onClick = { /*TODO*/ }
+            ) {
+                Icon(Icons.Outlined.Add, "Add a travel")
+            }
+        },
+        topBar = { AppBar(title = "TravelDiary") }
+    ) { contentPadding ->
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(2),
+            verticalArrangement = Arrangement.spacedBy(8.dp),
+            horizontalArrangement = Arrangement.spacedBy(8.dp),
+            contentPadding = PaddingValues(8.dp, 8.dp, 8.dp, 80.dp),
+            modifier = Modifier.padding(contentPadding)
+        ) {
+            items(items) { item -> TravelItem(item) }
         }
-        ScrollableList()
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun ScrollableList() {
-    val elems = (0..20).toList().map { "Item n°$it" }
-    
-    LazyColumn {
-        items(elems) {
-            Card(
-                colors = CardDefaults.cardColors(
-                    containerColor = MaterialTheme.colorScheme.primaryContainer,
-                ),
+fun TravelItem(item: String) {
+    Card(
+        onClick = { /*TODO*/ },
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.secondaryContainer
+        ),
+        modifier = Modifier
+            .size(150.dp)
+            .fillMaxSize()
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                Icons.Outlined.Image,
+                "Travel picture",
+                contentScale = ContentScale.Fit,
+                colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.onSecondary),
                 modifier = Modifier
-                    .size(width = 240.dp, height = 100.dp)
-            ) {
-                Text(
-                    it,
-                    modifier = Modifier
-                        .padding(16.dp),
-                    textAlign = TextAlign.Center,
-                )
-            }
+                    .size(72.dp)
+                    .clip(CircleShape)
+                    .background(MaterialTheme.colorScheme.secondary)
+                    .padding(20.dp)
+            )
+            Spacer(Modifier.size(8.dp))
+            Text(
+                text = item,
+                color = MaterialTheme.colorScheme.onSecondaryContainer,
+                style = MaterialTheme.typography.bodyMedium,
+                textAlign = TextAlign.Center
+            )
         }
     }
 }
--- Programmazione_Mobile/TravelDiary/app/src/main/java/com/example/traveldiary/ui/screens/TravelDetailsScreen.kt
+++ Programmazione_Mobile/TravelDiary/app/src/main/java/com/example/traveldiary/ui/screens/TravelDetailsScreen.kt
@@ -1,2 +1,81 @@
 package com.example.traveldiary.ui.screens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Image
+import androidx.compose.material.icons.outlined.Share
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
+import com.example.traveldiary.ui.composables.AppBar
+
+@Composable
+fun TravelDetailsScreen() {
+    Scaffold(
+        floatingActionButton = {
+            FloatingActionButton(
+                containerColor = MaterialTheme.colorScheme.primary,
+                onClick = { /*TODO*/ }
+            ) {
+                Icon(Icons.Outlined.Share, "Share the travel")
+            }
+        },
+        topBar = { AppBar(title = "Travel Details") }
+    ) { contentPadding ->
+        Column(
+            modifier = Modifier
+                .padding(contentPadding)
+                .padding(12.dp)
+                .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(12.dp)
+        ) {
+            Image(
+                Icons.Outlined.Image,
+                "Travel picture",
+                contentScale = ContentScale.Fit,
+                colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.onSecondary),
+                modifier = Modifier
+                    .size(140.dp)
+                    .clip(CircleShape)
+                    .background(MaterialTheme.colorScheme.secondary)
+                    .padding(36.dp)
+                    .padding(vertical = 16.dp),
+            )
+            Text(
+                "Destination",
+                color = MaterialTheme.colorScheme.onPrimaryContainer,
+                style = MaterialTheme.typography.titleLarge
+            )
+            Text(
+                "01/01/2024",
+                color = MaterialTheme.colorScheme.onPrimaryContainer,
+                style = MaterialTheme.typography.bodySmall
+            )
+            Spacer(Modifier.size(8.dp))
+            Text(
+                "Description",
+                color = MaterialTheme.colorScheme.onPrimaryContainer,
+                style = MaterialTheme.typography.bodyMedium
+            )
+        }
+
+    }
+}
